328075ab4932cbb751b0ea7f91e8a5afcf6c2600
Felipe Castro <felipe360@gmail.com>
2022-04-11 20:55:48 -0300

    Add Async exercises

---
+++ AsyncFunctions/src/index.js
@@ -0,0 +1,17 @@
+import "core-js"
+import "regenerator-runtime/runtime"
+
+import Spaceship from "./spaceship";
+import SpaceshipEngine from "./spaceship_engine"
+
+const sophia = new Spaceship("Sophia", 10, 5, 70)
+const amsterda = new Spaceship("Amsterda", 14, 10, 40)
+const dwarfStar = new Spaceship("Estrela-Ana", 20, 4, 80)
+
+const sophiaEngine = new SpaceshipEngine(sophia)
+const amsterdaEngine = new SpaceshipEngine(amsterda)
+const dwarfStarEngine = new SpaceshipEngine(dwarfStar)
+
+sophiaEngine.turnOn()
+amsterdaEngine.turnOn()
+dwarfStarEngine.turnOn()
