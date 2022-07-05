17032fb0adc2cf6b047494f618777d50bc957f7f
Felipe Castro <felipe3660@gmail.com>
2022-07-05 19:58:39 -0300

    add Components state

--- exercicioComponentes/ComponentesEx/src/components/planets/index.jsx
+++ exercicioComponentes/ComponentesEx/src/components/planets/index.jsx
@@ -1,25 +1,81 @@
 import React, { Fragment } from 'react';
 import Planet from './planet';
 
+const clickOnPlanet = (name) => {
+    console.log(`Um click no planeta: ${name}`)
+}
 
-const Planets = () => {
-    return (
-        <Fragment>
-            <h3> Planet List</h3>
-            <hr/>
-            <Planet name="Mercúrio"
-                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, nam soluta. Dolores quam quia sapiente, sunt dicta mollitia nulla temporibus."
-                    img_url="./images/jupiter.jpg"
-                    planet_link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-            />
-            <hr/>
-            <Planet name="Júpiter"
-                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptate eius pariatur aliquam molestias tenetur consectetur nobis cupiditate eveniet laboriosam dignissimos fugit, molestiae laborum quibusdam esse soluta amet praesentium? Sint!"
-                    img_url="https://pt.wikipedia.org/wiki/J%C3%BApiter_%28planeta%29#/media/Ficheiro:Jupiter_and_its_shrunken_Great_Red_Spot.jpg"
-                    planet_link="https://pt.wikipedia.org/wiki/J%C3%BApiter_(planeta)"
-            />
-        </Fragment>
-    )
+async function getPlanets() {
+    let response = await fetch('http://localhost:3000/api/planets.json')
+    let data = await response.json()
+    return data;
+}
+
+class Planets extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            planets: []
+            /*planets: [
+                {
+                    name: "Mercúrio",
+                    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, nam soluta. Dolores quam quia sapiente, sunt dicta mollitia nulla temporibus.",
+                    img_url: "./images/jupiter.jpg",
+                    link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
+                },
+                {
+                    name: "Júpiter",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptate eius pariatur aliquam molestias tenetur consectetur nobis cupiditate eveniet laboriosam dignissimos fugit, molestiae laborum quibusdam esse soluta amet praesentium? Sint!",
+                    img_url: "https://pt.wikipedia.org/wiki/J%C3%BApiter_%28planeta%29#/media/Ficheiro:Jupiter_and_its_shrunken_Great_Red_Spot.jpg",
+                    link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
+                }
+            ] */
+        }
+    }
+
+    componentDidMount() {
+        getPlanets().then(data => {
+            this.setState(state => ({
+                planets: data['planets']
+            }))
+        })
+    }
+
+    removeLast = () => {
+        let new_planets = [...this.state.planets]
+        new_planets.pop()
+        this.setState(state => ({
+            planets: new_planets
+        }))
+    }
+
+    duplicateLast = () => {
+        let last_planet = this.state.planets[this.state.planets.length - 1]
+        this.setState(state => ({
+            planets: [...this.state.planets, last_planet]
+        }))
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h3> Planet List</h3>
+                <button onClick={this.removeLast}>Remover o ultimo planeta!</button>
+                <button onClick={this.duplicateLast}>Duplicar o ultimo planeta!</button>
+                {this.state.planets.map((planet, index) =>
+                    <Planet
+                        key={index}
+                        id={planet.id}
+                        name={planet.name}
+                        description={planet.description}
+                        img_url={planet.img_url}
+                        link={planet.link}
+                        clickOnPlanet={clickOnPlanet}
+                    />
+                )}
+            </Fragment>
+        )
+    }
 }
 
 export default Planets;
--- exercicioComponentes/ComponentesEx/src/components/planets/planet/index.jsx
+++ exercicioComponentes/ComponentesEx/src/components/planets/planet/index.jsx
@@ -1,15 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import GrayImage from '../../shared/gray-image';
 import DescriptionWithLink from '../../shared/gray-image/DescriptionLink';
 
-const Planet = (props) => {
-    return (
-        <div>
-            <h4>{props.name}</h4>
-            <DescriptionWithLink description={props.description} link={props.planet_link}/>
-            <GrayImage img_url={props.img_url}/>
-        </div>
-    )
+async function getSat(id) {
+    let response = await fetch(`http://localhost:3000/api/${id}.json`)
+    let data = await response.json();
+    return data;
 }
+
+class Planet extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            satellites: []
+        }
+    }
+
+    componentDidMount() {
+        getSat(this.props.id).then(data => {
+            this.setState(state => ({
+                satellites: data['satellites']
+            }))
+        })
+    }
+
+
+    render() {
+        let title;
+        if (this.props.title_with_underline) {
+            title = <h4><u>{this.props.name}</u></h4>
+        } else {
+            title = <h4>{this.props.name}</h4>
+        }
+
+        return (
+            <div onClick={() => props.clickOnPlanet(this.props.name)}>
+                {title}
+                <DescriptionWithLink description={this.props.description} link={this.props.planet_link} />
+                <GrayImage img_url={this.props.img_url} />
+                <h4>Satélites</h4>
+                <ul>
+                    {this.state.satellites.map((satellites, index) =>
+                        <li key={index}>{satellites.name}</li>
+                    )}
+                </ul>
+                <hr />
+            </div>
+        )
+    }
+}
+
+
 
 export default Planet;
